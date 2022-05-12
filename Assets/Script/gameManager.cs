60ffcc70b03bb57bf633413b01309d4e81d1fd12
arian driussi <arian.driussi.dev@gmail.com>
2022-05-12 01:12:43 -0300

    Game Manager

---
+++ Assets/Script/gameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState {
+    menu,
+    inGame,
+    gameOver
+}
+public class gameManager : MonoBehaviour
+{
+
+    public GameState currentGameState = GameState.menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void EndGame()
+    {
+
+    }
+
+    public void BackToMenu()
+    {
+
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+            // colocar la lógica del menu
+        } else if (newGameState == GameState.inGame)
+        {
+
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            // preparar el juego para el game over
+
+        }
+
+     this.currentGameState = newGameState;
+    }
+}
